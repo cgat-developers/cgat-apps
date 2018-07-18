34bb8f6a496d3baf33b24d1f840f8d367aa8d5fe
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2018-07-18 13:40:18 +0100

    enforce sqlite=3.20.1=2 ; https://github.com/conda/conda/issues/7553

--- install-CGAT-tools.sh
+++ install-CGAT-tools.sh
@@ -415,10 +415,7 @@ if [[ -z ${TRAVIS_INSTALL} ]] ; then
       [[ $CODE_DOWNLOAD_TYPE -ge 1 ]] && git checkout -- setup.py
 
       # environment pinning
-      #python scripts/conda.py
-      # temp workaround; problem with sqlite3 and osx
-      #sed -i'' -e '/sqlite/d' ${CONDA_INSTALL_DIR}/envs/${CONDA_INSTALL_ENV}/conda-meta/pinned
-      conda update -n ${CONDA_INSTALL_ENV} sqlite --yes --no-deps
+      python scripts/conda.py
 
    fi # if INSTALL_DEVEL
 
