5eee2cb8f9618703f7edf847f86dab2a4df4dd4f
Acribbs <a.p.cribbs@gmail.com>
2024-10-25 14:16:54 +0100

    changed testing to pytests

--- tests/test_style.py
+++ tests/test_style.py
@@ -1,7 +1,7 @@
 '''test_style - test coding style confirmation of CGAT code
 ===========================================================
 
-:Author: Andreas Heger
+:Author: Adam Cribbs
 :Release: $Id$
 :Date: |today|
 :Tags: Python
@@ -20,65 +20,41 @@ This script is best run within nosetests::
 import pep8
 import glob
 import os
-from nose.tools import ok_
 
 # DIRECTORIES to examine for python modules/scripts
 EXPRESSIONS = (
     ('tests', 'tests/*.py'),
     ('scripts', 'cgat/tools/*.py'),
-    ('cgat', 'cgat/*.py'))
+    ('cgat', 'cgat/*.py')
+)
 
 # Codes to ignore in the pep8 BaseReport
-IGNORE = set(('E101',  # indentation contains mixed spaces and tabs
-              'E201',  # whitespace after '('
-              'E202',  # whitespace before ')'
-              'E122',  # continuation line missing indentation or outdented
-              'E265',  # block comment should start with '# '
-              'E501',  # line too long (82 > 79 characters)
-              'E502',  # the backslash is redundant between brackets
-              'E731',  # do not assign a lambda expression, use a def
-              'W191',
-              'W291',
-              'W293',
-              'W391',
-              'W503',  # line break before binary operator
-              'W601',
-              'W602',
-              'files',
-              'directories',
-              'physical lines',
-              'logical lines',))
+IGNORE = set(('E101', 'E201', 'E202', 'E122', 'E265', 'E501', 'E502', 'E731',
+              'W191', 'W291', 'W293', 'W391', 'W503', 'W601', 'W602', 'E701', 'E713', 'E702'))
 
 
 def check_style(filename):
-    '''check style of filename.
-    '''
+    '''check style of filename.'''
 
     p = pep8.StyleGuide(quiet=True)
     report = p.check_files([filename])
 
-    # count errors/warning excluding
-    # those to ignore
-    take = [y for x, y
-            in report.counters.items() if x not in IGNORE]
-    found = ['%s:%i' % (x, y) for x, y
-             in report.counters.items() if x not in IGNORE]
+    # Exclude non-violation counters from the result
+    take = [y for x, y in report.counters.items() if x not in IGNORE and x not in ('files', 'directories', 'physical lines', 'logical lines')]
+    found = ['%s:%i' % (x, y) for x, y in report.counters.items() if x not in IGNORE and x not in ('files', 'directories', 'physical lines', 'logical lines')]
     total = sum(take)
-    ok_(total == 0,
-        'pep8 style violations in %s: %s' % (filename, ','.join(found)))
+
+    assert total == 0, f'pep8 style violations in {filename}: {", ".join(found)}'
 
 
 def test_style():
-    '''test style of scripts
-    '''
+    '''test style of scripts'''
 
     for label, expression in EXPRESSIONS:
-
         files = glob.glob(expression)
         files.sort()
 
         for f in files:
             if os.path.isdir(f):
                 continue
-            check_style.description = os.path.abspath(f)
-            yield(check_style, os.path.abspath(f))
+            check_style(os.path.abspath(f))
